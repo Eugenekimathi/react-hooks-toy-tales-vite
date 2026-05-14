c2b3f3e662a98e054f2dcd8fef6b594465dd8b75
Eugene Kimathi <eugenekimathi@gmail.com>
2026-05-14 22:58:35 +0300

    Implement add toy features

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,55 +1,20 @@
-// import React from "react";
+import React,{useState} from "react";
 
-// function ToyForm() {
-//   return (
-//     <div className="container">
-//       <form className="add-toy-form">
-//         <h3>Create a toy!</h3>
-//         <input
-//           type="text"
-//           name="name"
-//           placeholder="Enter a toy's name..."
-//           className="input-text"
-//         />
-//         <br />
-//         <input
-//           type="text"
-//           name="image"
-//           placeholder="Enter a toy's image URL..."
-//           className="input-text"
-//         />
-//         <br />
-//         <input
-//           type="submit"
-//           name="submit"
-//           value="Create New Toy"
-//           className="submit"
-//         />
-//       </form>
-//     </div>
-//   );
-// }
+function ToyForm({onAddToy}) {
+  const [name,setName] = useState ("")
+  const [image,setImage] = useState("")
 
-// export default ToyForm;
+  function handleSubmit(e){
+    e.preventDefault()
+    fetch("http://localhost:3000/toys",{
+      method:"POST",
+      headers:{"Content-Type": "application/json"},
+      body:JSON.stringify({name,image,likes:0})
 
-
-import React, { useState } from "react";
-
-function ToyForm({ onAddToy }) {
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("http://localhost:3000/toys", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, image, likes: 0 })
     })
-      .then(res => res.json())
-      .then(newToy => onAddToy(newToy));
+    .then(res => res.json())
+    .then(newToy => onAddToy(newToy))
   }
-
   return (
     <div className="container">
       <form className="add-toy-form" onSubmit={handleSubmit}>
