e51b48dea11a34931f6a8a27cac3ba916a623c64
Eugene Kimathi <eugenekimathi@gmail.com>
2026-05-14 23:14:28 +0300

    Implement donate and like features

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,29 +1,17 @@
-// import React from "react";
-// import ToyCard from "./ToyCard";
-
-// function ToyContainer() {
-//   return (
-//     <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
-//   );
-// }
-
-// export default ToyContainer;
-
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ toys, onUpdateToy, onDeleteToy }) {
+function ToyContainer({toys,onUpdateToy,onDeleteToy}) {
   return (
-    <div id="toy-collection">
-      {toys.map(toy => (
+    <div id="toy-collection">{
+      toys.map(toy => (
         <ToyCard
-          key={toy.id}
-          toy={toy}
-          onUpdateToy={onUpdateToy}
-          onDeleteToy={onDeleteToy}
-        />
-      ))}
-    </div>
+        key={toy.id}
+        toy={toy}
+        onUpdateToy={onUpdateToy}
+        onDeleteToy={onDeleteToy}/>
+      ))
+    }</div>
   );
 }
 
