8f9784b255a35586878e014e25e1ca88b65da8ff
Eugene Kimathi <eugenekimathi@gmail.com>
2026-05-14 22:22:29 +0300

    Implement display all toys feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,21 +6,41 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys,setToys] = useState([])
 
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
+  useEffect(()=> { 
+    fetch("http://localhost:3000/toys")
+    .then (res => res.json())
+    .then(data => setToys(data))
+  },[])
+
+  function handleAddToy(newToy){
+    setToys([...toys,newToy])
+  }
+
+  function handleUpdateToy(updatedToy){
+    setToys(toys.map(t=>t.id === updatedToy.id ? updatedToy: t))
+  }
+
+  function handleDeleteToy(id){
+    setToys(toys.filter(t=> t.id !==id))
   }
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
+        <button onClick={()=>setShowForm(show => !show)}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+      toys={toys}
+      onUpdateToy={handleUpdateToy}
+      onDeleteToy={handleDeleteToy}
+      />
     </>
   );
 }
 
 export default App;
+
